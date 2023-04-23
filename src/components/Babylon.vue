e02bb45ec3020b2eb9916ac04f45638327ba9427
yanin-artem <108901382+yanin-artem@users.noreply.github.com>
2023-04-23 21:36:57 +0300

    Создание базовой сцены (камера, свет, пол, шар)

---
+++ src/components/Babylon.vue
@@ -0,0 +1,21 @@
+<template>
+  <canvas style="width: 80%;height: 80%;" ref="canvas" />
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Scene from "src/babylon/Scene";
+
+const canvas = ref();
+
+onMounted(() => {
+  new Scene(canvas.value);
+})
+
+</script>
+
+<style lang="sass" scoped>
+canvas
+  width: 80%
+  height: 80%
+</style>
